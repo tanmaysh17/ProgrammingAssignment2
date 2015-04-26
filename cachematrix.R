a612b498b2a2e8f4c93e65fa59ab153293b9c0b6
tanmaysh17 <tanmay.sh17@gmail.com>
2015-04-26 22:09:39 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,14 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-
-
 ## Programming Assignement 2
+# makeCacheMatrix is a function that returns a list of functions (as describer below). 
+# It's purpose is to store/cache a matrix and it's inverse value.
 
+# setMatrix     -       Sets the new matrix for which inverse has to be cached
+# getMatrix     -       Prints the matrix, inverse for which is to be cached
+# setInverse    -       Caches the invserse of the matrix
+# getinverse    -       Returns the cached value
+
+# makeCacheMatrix function
 makeCacheMatrix <- function(x = matrix()) {
 
-
-
+        # Hold the cached value, initially nothing hence NULL
+        Inv <- NULL
+        
+        # Sets in a new value for a matrix for which inverse is to be cached
+        setMatrix <- function(y){
+                x <<- y
+                #flush the cache, as the matrix now has a new value
+                Inv <<- NULL
+        }
+        
+        # Returns the stored matrix
+        getMatrix <- function() x
+        
+        # Cache the given inverse of the matrix
+        setInverse <- function(inverse) Inv <<- inverse
+        
+        # Get the cached value of inverse of the matrix
+        getInverse <- function() Inv
+        
+        # Return a list of functions of the same name as their purpose
+        List(setMatrix = setMatrix, getMatrix = getMatrix, setInverse = setInverse, getInverse = getInverse)
 }
 
 
